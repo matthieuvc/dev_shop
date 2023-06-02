4b60c36a71368a9f59e24db1d207b946d8e710c4
Charlotte Nirenberg <charloutte19999@gmail.com>
2023-06-02 12:51:42 +0200

    a lot of small issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_105643) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_091145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_105643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.text "description"
     t.index ["user_id"], name: "index_webdevs_on_user_id"
   end
 
