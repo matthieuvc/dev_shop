1bfbd9a56a5dd4efae887e2f076b49365eb62ce3
matthieu van cauwenbergh <vancauwenbergh.matthieu@gmail.com>
2023-06-02 12:51:47 +0200

    checkmark and login

--- app/controllers/webdevs_controller.rb
+++ app/controllers/webdevs_controller.rb
@@ -1,4 +1,7 @@
 class WebdevsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @webdevs = Webdev.all
     if params[:query].present?
